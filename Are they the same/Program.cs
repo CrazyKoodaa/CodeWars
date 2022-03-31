81c6f12abe4942c3fae6156a22c7ea6a0890d638
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-31 12:09:21 +0200

    Are they the same and Maximum Subarray Challenge finished

---
+++ Are they the same/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AreTheySame
+{
+    public static bool comp(int[] a, int[] b)
+    {
+        if (a?.Length != b?.Length) return false;
+
+        List<int> listA = a.ToList();
+        List<int> listB = b.ToList();
+
+        foreach (var x in listB)
+        {
+
+            // if (x is null) return false;
+            int temp = (int)Math.Pow((double)x, 0.5f);
+            if (!listA.Contains(temp))
+                return false;
+            else
+                listA.Remove(temp);
+
+        }
+        return true;
+    }
+}
---
+++ Training on Maximum subarray sum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Training_on_Maximum_subarray_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (Kata.MaxSequence(new int[0]) == 0)
+                Console.WriteLine("test 1 okay");
+            if (Kata.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }) == 6)
+                Console.WriteLine("test 2 okay");
+
+        }
+    }
+
+    public static class Kata
+    {
+        public static int MaxSequence(int[] arr)
+        {
+            //TODO : create code
+            if (arr.Length == 0)
+                return 0;
+
+            int sum = 0, max = 0; //pos1 = 0, pos2 = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = 0;
+                for (int x = i; x < arr.Length; x++)
+                {
+                    sum += arr[x];
+                    if (sum > max)
+                    {
+                        max = sum;
+                        //pos1 = i;
+                        //pos2 = x;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Max = {max}");
+            return max;
+        }
+    }
+}
