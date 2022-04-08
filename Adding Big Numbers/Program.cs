113654230408102b413fc7f1bea09ea191ddb135
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-08 16:58:51 +0200

    Adding Big Numbers - Oneliner

---
+++ Adding Big Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static string Add(string a, string b)
+    {
+        // Fix this!
+        return (BigInteger.Add(BigInteger.Parse(a), BigInteger.Parse(b))).ToString();
+
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Add("555", "555"));
+    }
+} 
