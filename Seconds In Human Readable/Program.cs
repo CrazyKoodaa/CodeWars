b07c7d0bb5d10c882187b56029f0456f39767556
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-08 17:21:26 +0200

    Seconds to hours:minutes:seconds

---
+++ Seconds In Human Readable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Seconds_In_Human_Readable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(TimeFormat.GetReadableTime(359999));
+        }
+    }
+
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            TimeSpan ts = TimeSpan.FromSeconds(seconds);
+
+            return String.Format($"{(int)ts.TotalHours:D2}:{ts.Minutes:D2}:{ts.Seconds:D2}");
+        } 
+    }
+
+}
