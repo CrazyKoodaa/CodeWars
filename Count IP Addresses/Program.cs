41fc0776b0796156da881b2a335c05a08cd3ad3a
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-09 14:30:54 +0200

    Subtract Two IPs and show howmany clients are there

---
+++ Count IP Addresses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+
+public class CountIPAddresses
+{
+
+    public static long IpsBetween(string start, string end)
+    {
+
+        #region MySolution
+        /*
+        var startNumber = start.Split('.');
+        var endNumber = end.Split('.');
+
+        long iStart = 0, iEnd = 0;
+
+        for (double i = 0, z = 3; i < startNumber.Length; i++, z--)
+        {   
+            iStart += (long)(Convert.ToInt64(startNumber[(int)i]) * Math.Pow(256, z));
+            iEnd += (long)(Convert.ToInt64(endNumber[(int)i]) * Math.Pow(256, z));
+        }
+
+        return iEnd - iStart;
+        */
+        #endregion
+
+        #region BetterSolution
+        
+
+        return (long)(uint)IPAddress.NetworkToHostOrder((int)IPAddress.Parse(end).Address) -
+            (long)(uint)IPAddress.NetworkToHostOrder((int)IPAddress.Parse(start).Address);
+
+        #endregion
+
+
+    }
+}
+
+namespace Count_IP_Addresses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(CountIPAddresses.IpsBetween("10.0.0.0", "10.0.0.50"));
+            Console.WriteLine(CountIPAddresses.IpsBetween("20.0.0.10", "20.0.1.0"));
+            Console.WriteLine(CountIPAddresses.IpsBetween("0.0.0.0", "255.255.255.255"));
+        }
+    }
+}
