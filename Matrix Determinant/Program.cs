44b81d491e716d45b3dd7a3fbb28ab750f1bdeff
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-10 15:59:43 +0200

    Sum of Intervals Solution for Codewars's Katas

---
+++ Matrix Determinant/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+namespace Matrix_Determinant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Matrix myMatrix = new Matrix(5, 10, 15, 20, 25, 30);
+
+            // Get the determinant, which is equal to -50.
+            Double determinant = myMatrix.Determinant;
+
+            Console.WriteLine(determinant);
+        }
+    }
+}
---
+++ Sum of Intervals/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Interval = System.ValueTuple<int, int>;
+using System.Collections.Generic;
+
+namespace Sum_of_Intervals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            #region MySolution
+            /*
+            Tuple<int,int>[] riding = new[] { new Tuple<int,int>(1, 2), new Tuple<int,int>(2, 6), new Tuple<int, int>(6, 55) };
+            Dictionary<int,int> counts = new Dictionary<int,int>();
+            
+            foreach (var i in riding)
+                for (int j = i.Item1; j < i.Item2; j++)
+                    counts.TryAdd(j, i.Item2);
+
+            Console.WriteLine(counts.Count);
+            */
+            #endregion
+
+            #region LinqVersion
+            return intervals
+              .SelectMany(i => Enumerable.Range(i.Item1, i.Item2 - i.Item1))
+              .Distinct()
+              .Count();
+            #endregion
+
+        }
+    }
+
+}
